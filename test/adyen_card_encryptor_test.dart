d6ebb0c1d7af9e3bec5728aa1209e8ddd3832724
Dawid Oliveira <dawid.oliveira@kobe.io>
2024-01-26 11:50:42 -0300

    # config: upgrade libs

--- test/adyen_card_encryptor_test.dart
+++ test/adyen_card_encryptor_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:adyen_card_encryptor/adyen_card_encryptor.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('adyen_card_encryptor');
