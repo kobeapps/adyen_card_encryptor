d8a09bacc32dad4b3e2ef0c17b9f83d275a7b086
Adby Santos <adby.santos@kobe.io>
2024-11-21 10:06:18 -0300

    chore: updates android folder and pubspec

--- example/android/app/src/main/kotlin/com/example/adyen_card_encryptor_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/adyen_card_encryptor_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.adyen_card_encryptor_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
